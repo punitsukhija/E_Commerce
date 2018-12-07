071bd564e9737eadfb438f2a73aa4543a458f0a7
karunaSemlr <karuna.bhardwaj@semlr.com>
2018-12-07 16:32:33 +0530

    new commit

--- src/main/java/com/wissen/e_commerce/resource/OrderResource.java
+++ src/main/java/com/wissen/e_commerce/resource/OrderResource.java
@@ -10,35 +10,44 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.wissen.e_commerce.exception.E_CommerceException;
 import com.wissen.e_commerce.pojo.OrderBo;
+import com.wissen.e_commerce.service.OrderService;
 
 @Component
 @Path("/order")
 public class OrderResource {
 	
+	@Autowired
+	private OrderService orderService;
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response newOrder(OrderBo orderBo) {
+	public Response newOrder(OrderBo orderBo) throws E_CommerceException {
+		orderService.placeOrder(orderBo);
 		return Response.ok().build();
 	}
 	
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateOrder(OrderBo orderBo) {
+	public Response updateOrder(OrderBo orderBo) throws E_CommerceException {
+		orderService.modifyOrder(orderBo);
 		return Response.ok().build();
 	}
 	
 	@DELETE
 	@Path("{id}")
-	public Response cancelOrder(@PathParam("id")Long id) {
+	public Response cancelOrder(@PathParam("id")Long id) throws E_CommerceException {
+		orderService.cancelOrder(id);
 		return Response.ok().build();
 	}
 	
 	@GET
 	@Path("{id}")
-	public Response checkOrderStatus(@PathParam("id")Long id) {
+	public Response checkOrderStatus(@PathParam("id")Long id) throws E_CommerceException {
+		orderService.checkOrderStatus(id);
 		return Response.ok().build();
 	}
 
